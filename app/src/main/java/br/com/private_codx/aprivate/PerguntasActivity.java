2ba991357ae3595aed643d167a0936ba99dfe56e
felipe <wfelipewill@hotmail.com>
2017-03-04 03:10:57 -0300

    Adicionado tela de Perguntas

---
+++ app/src/main/java/br/com/private_codx/aprivate/PerguntasActivity.java
@@ -0,0 +1,13 @@
+package br.com.private_codx.aprivate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PerguntasActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perguntas);
+    }
+}
