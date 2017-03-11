e3aa32177c6f80b8efee873e21a8bfd0753d02a8
will felipe <wfelipewill@hotmail.com>
2017-03-10 21:52:33 -0300

    foi adicionado botão que chama a ela perguntasActivity

--- app/src/main/java/br/com/private_codx/aprivate/ActivityPrincipal.java
+++ app/src/main/java/br/com/private_codx/aprivate/ActivityPrincipal.java
@@ -1,13 +1,27 @@
 package br.com.private_codx.aprivate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityPrincipal extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        Button btnPerguntas = (Button) findViewById(R.id.btnPerguntass);
+        btnPerguntas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(getApplicationContext(), PerguntasActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
