b306e09797fa8a01910a09d7825938ddcd2628a9
alissonxw <alisson-alves-de-souza@hotmail.com>
2017-03-12 17:15:38 -0300

    adição do spinnerorder

--- app/src/main/java/br/com/private_codx/aprivate/ActivityPesquisa.java
+++ app/src/main/java/br/com/private_codx/aprivate/ActivityPesquisa.java
@@ -20,6 +20,16 @@ public class ActivityPesquisa extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+
+        Spinner spinnerorder = (Spinner) findViewById(R.id.order);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.order, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinnerorder.setAdapter(adapter1);
     }
 
 }
