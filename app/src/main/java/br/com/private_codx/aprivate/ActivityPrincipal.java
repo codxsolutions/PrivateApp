ad6b7da02d3e4adc5830a4c1e96c7104f77083b9
Thiago <thiago197garcia@gmail.com>
2017-03-15 02:20:45 -0300

    Começo da implementação do Menu da ActionBar!

--- app/src/main/java/br/com/private_codx/aprivate/ActivityPrincipal.java
+++ app/src/main/java/br/com/private_codx/aprivate/ActivityPrincipal.java
@@ -3,6 +3,8 @@ package br.com.private_codx.aprivate;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -33,6 +35,33 @@ public class ActivityPrincipal extends AppCompatActivity {
             }
         });
 
+    }
 
+    //Contextual Menu
+    //Chama o menu na Activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //Seleciona os itens do menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_configuracoes) {
+            return true;
+        }
+
+        if (id == R.id.action_logout) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
