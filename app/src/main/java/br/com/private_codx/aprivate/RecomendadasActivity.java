b93405d71ca6258478135da1403b265a8f0b3eb3
Thiago Rocha <thiago197garcia@gmail.com>
2017-03-31 20:00:25 -0300

    Criação de todas as Activitys

---
+++ app/src/main/java/br/com/private_codx/aprivate/RecomendadasActivity.java
@@ -0,0 +1,13 @@
+package br.com.private_codx.aprivate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecomendadasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recomendadas);
+    }
+}
