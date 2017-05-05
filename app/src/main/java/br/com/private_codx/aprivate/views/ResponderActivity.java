e5cf0f8c48d35b0d2f898876cfcfaabc86dd5e58
Thiago Rocha <thiago197garcia@gmail.com>
2017-05-05 19:49:47 -0300

    Corrigindo o inseto

--- app/src/main/java/br/com/private_codx/aprivate/views/ResponderActivity.java
+++ app/src/main/java/br/com/private_codx/aprivate/views/ResponderActivity.java
@@ -2,18 +2,30 @@ package br.com.private_codx.aprivate.views;
 /**
  * Created by Thiago on 03/05/2017.
  */
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import br.com.private_codx.aprivate.R;
 
 public class ResponderActivity extends AppCompatActivity {
     //Variáveis
     private Toolbar aToolbar; //Declarando a Toolbar
-
+    //Contador de caracteres limite
+    private TextView txtMensagem;
+    private int contCaracteres = 150;
+    private EditText editResposta;
+    private Button btnEnviar;
+    private String conteudo = "";
+    private int tamanho = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +35,49 @@ public class ResponderActivity extends AppCompatActivity {
         //Toolbar
         aToolbar = (Toolbar) findViewById(R.id.toolbar_responder);
         setSupportActionBar(aToolbar);
+
+        //CExibindo quantidade de caracteres
+        txtMensagem = (TextView) findViewById(R.id.txtMensagem);
+        txtMensagem.setText("" + contCaracteres + " Caracteres disponíveis");
+        editResposta = (EditText) findViewById(R.id.editResposta);
+        btnEnviar = (Button) findViewById(R.id.btnEnviarResposta);
     }
+
+    //listener de quantidade de caracteres
+    TextWatcher onSearchFieldTextChanged = new TextWatcher() {
+        public void afterTextChanged(Editable s) {
+
+        }
+
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            int quantidade = editResposta.getText().length();
+            int atual = contCaracteres - quantidade;
+
+            if (atual > 0) {
+                String valor = String.valueOf(atual);
+                txtMensagem.setText(valor + " Caracteres disponíveis");
+                txtMensagem.setTextColor(Color.rgb(255, 255, 255));
+            } else if (atual == 0) {
+                txtMensagem.setText("Quantidade máxima de caracteres");
+                txtMensagem.setTextColor(Color.rgb(255, 255, 255));
+                conteudo = editResposta.getText().toString();
+            } else {
+                editResposta.setText(conteudo);
+                txtMensagem.setTextColor(Color.rgb(255, 0, 0));
+                txtMensagem.setText("Quantidade máxima de caracteres excedida!");
+            }
+
+        }
+    };
+    // fim do listener
+
+
+
+
 
     //MENU
     @Override
